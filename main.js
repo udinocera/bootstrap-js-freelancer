052f43475def7ee21321f1cef917727a2b009bdf
udinocera <udinocera@gmail.com>
2022-03-29 14:42:40 +0200

    Inizio Script

---
+++ main.js
@@ -0,0 +1,15 @@
+/*  - Se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.5€ l’ora
+    - Se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.3€ l’ora
+    - Se la commissione riguarda l’analisi progettuale di un progetto il prezzo orario è di 33.6€
+*/
+
+const back_end = 20.5;
+const front_end = 15.3;
+const project_analist = 33.6;
+
+/*
+Se l'utente utilizza un codice promozionale tra i seguenti (YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24)
+Fate in modo che l’utente abbia diritto ad uno sconto del 25% sul prezzo finale.
+*/
+
+let codice_promozionale = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
